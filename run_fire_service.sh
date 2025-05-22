8382fbd0aad044dd5bb26154ddbbd75eaeb7cd38
Rauli Laitinen <rauli.laitinen@aalto.fi>
2025-05-23 00:50:26 +0300

    Add local run script

---
+++ run_fire_service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source ~/flask_env/bin/activate
+
+# Change to the project directory (optional, if not already there)
+cd /Users/andik/Downloads/fire_service
+
+# Run the Flask app
+python app.py
